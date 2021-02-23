a75cbc16e8929c4af1b4364a546a50af8a0faf8a
Rob Nelson <robert.nelson@duke.edu>
2021-02-23 11:12:07 -0500

    add more error returning to callbacks

--- stash.go
+++ stash.go
@@ -1,10 +1,11 @@
 package staging_importer
 
 import (
+	"errors"
 	"fmt"
 )
 
-type IntakeListMaker func(int) []Storeable
+type IntakeListMaker func(int) ([]Storeable, error)
 type ChunkableIntakeConfig struct {
 	Count     int
 	ChunkSize int
@@ -13,22 +14,29 @@ type ChunkableIntakeConfig struct {
 	ListMaker IntakeListMaker
 }
 
-func IntakeInChunks(ins ChunkableIntakeConfig) {
+func IntakeInChunks(ins ChunkableIntakeConfig) error {
+	var err error
 	for i := 0; i < ins.Count; i += ins.ChunkSize {
-		fmt.Printf("> retrieving %d-%d of %d\n", i, i+ins.ChunkSize, ins.Count)
-		list := ins.ListMaker(i)
+		// TODO: some way to print out status as running?  callback?
+		//fmt.Printf("> retrieving %d-%d of %d\n", i, i+ins.ChunkSize, ins.Count)
+		list, err := ins.ListMaker(i)
+		if err != nil {
+			return err
+		}
 		if !ins.JustTest {
-			err := BulkAddStaging(list...)
+			err = BulkAddStaging(list...)
 			if err != nil {
-				fmt.Println("could not save as list")
+				return err
 			}
 		} else {
+			// TODO: something better here for 'justTest'?
 			fmt.Printf("would save:%s", list)
 		}
 	}
+	return err
 }
 
-type OutakeListMaker func() []string
+type OutakeListMaker func() ([]string, error)
 type OutakeProcessConfig struct {
 	TypeName  string
 	ListMaker OutakeListMaker
@@ -36,28 +44,37 @@ type OutakeProcessConfig struct {
 }
 
 // TODO: shouldn't this return error if there is a problem?
-func ProcessOutake(proc OutakeProcessConfig) {
-	sourceData := proc.ListMaker()
-
+func ProcessOutake(proc OutakeProcessConfig) error {
+	sourceData, err := proc.ListMaker()
+	if err != nil {
+		msg := fmt.Sprintf("couldn't make list sent in for %s\n", proc.TypeName)
+		return errors.New(msg)
+	}
 	// NOTE: for comparing source data of *all* with existing *all*
 	err, resources := RetrieveTypeResources(proc.TypeName)
 	if err != nil {
-		fmt.Printf("couldn't retrieve list of %s\n", proc.TypeName)
+		msg := fmt.Sprintf("couldn't retrieve list of %s\n", proc.TypeName)
+		return errors.New(msg)
 	}
-	flagDeletes(sourceData, resources, proc.TypeName, proc.JustTest)
+	return flagDeletes(sourceData, resources, proc.TypeName, proc.JustTest)
 }
 
 type ExistingListMaker func() (error, []Resource)
 
-func ProcessCompare(proc DiffProcessConfig) {
+func ProcessCompare(proc DiffProcessConfig) error {
 	// NOTE: idea is to compare two limited lists such as overview per duid
 	// instead of looking for *all* extra overviews
-	sourceData := proc.ListMaker()
+	sourceData, err := proc.ListMaker()
+	if err != nil {
+		msg := fmt.Sprintf("couldn't make list sent in for %s\n", proc.TypeName)
+		return errors.New(msg)
+	}
 	err, resources := proc.ExistingListMaker()
 	if err != nil {
-		fmt.Printf("couldn't retrieve list of %s\n", proc.TypeName)
+		msg := fmt.Sprintf("couldn't retrieve list of %s\n", proc.TypeName)
+		return errors.New(msg)
 	}
-	flagDeletes(sourceData, resources, proc.TypeName, proc.JustTest)
+	return flagDeletes(sourceData, resources, proc.TypeName, proc.JustTest)
 }
 
 // to look for diffs for duid (for instance) both lists have to be sent in
@@ -68,17 +85,18 @@ type DiffProcessConfig struct {
 	JustTest          bool
 }
 
-func flagDeletes(sourceDataIds []string, existingData []Resource, typeName string, justTest bool) {
+func flagDeletes(sourceDataIds []string, existingData []Resource, typeName string, justTest bool) error {
 	destData := make([]string, 0)
 
 	if len(sourceDataIds) == 0 && len(existingData) > 0 {
-		fmt.Printf("0 source records found - this would delete all %s records!\n", typeName)
-		return
+		msg := fmt.Sprintf("0 source records found - this would delete all %s records!\n", typeName)
+		return errors.New(msg)
 	} else if len(sourceDataIds) > 0 && len(existingData) == 0 {
-		fmt.Println("no existing records to compare against")
+		msg := "no existing records to compare against"
+		return errors.New(msg)
 	} else if len(sourceDataIds) == 0 && len(existingData) == 0 {
-		fmt.Println("0 record to compare on either side!")
-		return
+		msg := "0 record to compare on either side!"
+		return errors.New(msg)
 	}
 
 	if len(existingData) > 0 {
@@ -86,8 +104,8 @@ func flagDeletes(sourceDataIds []string, existingData []Resource, typeName strin
 		// NOTE: function intent is to be comparing ids/per type - not just
 		// any list of ids
 		if peek.Type != typeName {
-			fmt.Printf("unexpected type in existing data (%s vs %s)!\n", peek.Type, typeName)
-			return
+			msg := fmt.Sprintf("unexpected type in existing data (%s vs %s)!\n", peek.Type, typeName)
+			return errors.New(msg)
 		}
 	}
 
@@ -96,6 +114,7 @@ func flagDeletes(sourceDataIds []string, existingData []Resource, typeName strin
 	}
 	extras := Difference(destData, sourceDataIds)
 
+	// TODO: maybe send count as return value
 	fmt.Printf("found %d extras\n", len(extras))
 	deletes := make([]Identifiable, 0)
 	for _, id := range extras {
@@ -104,12 +123,14 @@ func flagDeletes(sourceDataIds []string, existingData []Resource, typeName strin
 	}
 
 	if !justTest {
-		// NOTE: this is just marking them
+		// NOTE: this is just marking them, not deleting at this stage
 		err := BulkAddStagingForDelete(deletes...)
 		if err != nil {
-			fmt.Println("could not mark for delete")
+			msg := fmt.Sprintf("could not mark for delete: %s", err)
+			return errors.New(msg)
 		}
 	} else {
 		fmt.Printf("would mark these:%s\n", deletes)
 	}
+	return nil
 }
